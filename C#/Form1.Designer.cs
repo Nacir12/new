d72fa9308ec1443f5288b0cf6f9e67273c322f10
Nacir12 <123138855+Nacir12@users.noreply.github.com>
2023-04-24 08:10:47 -0400

    Add files via upload

--- C#/Form1.Designer.cs
+++ C#/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Programa_2._9
+﻿namespace Programa_3._9
 {
     partial class Form1
     {
@@ -28,87 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstFrutas = new System.Windows.Forms.ListBox();
-            this.txtFruta = new System.Windows.Forms.TextBox();
-            this.btnAdicionar = new System.Windows.Forms.Button();
-            this.lblFruta = new System.Windows.Forms.Label();
-            this.btnEliminar = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lstFrutas
+            // tableLayoutPanel1
             // 
-            this.lstFrutas.FormattingEnabled = true;
-            this.lstFrutas.Items.AddRange(new object[] {
-            "Manzana",
-            "Mora",
-            "Pera"});
-            this.lstFrutas.Location = new System.Drawing.Point(4, 102);
-            this.lstFrutas.Name = "lstFrutas";
-            this.lstFrutas.Size = new System.Drawing.Size(300, 95);
-            this.lstFrutas.TabIndex = 0;
-            this.lstFrutas.SelectedIndexChanged += new System.EventHandler(this.lstFrutas_SelectedIndexChanged);
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(24, 13);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 41F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 122);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // txtFruta
+            // button1
             // 
-            this.txtFruta.Location = new System.Drawing.Point(13, 26);
-            this.txtFruta.Name = "txtFruta";
-            this.txtFruta.Size = new System.Drawing.Size(100, 20);
-            this.txtFruta.TabIndex = 1;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // btnAdicionar
+            // button2
             // 
-            this.btnAdicionar.Location = new System.Drawing.Point(164, 22);
-            this.btnAdicionar.Name = "btnAdicionar";
-            this.btnAdicionar.Size = new System.Drawing.Size(75, 23);
-            this.btnAdicionar.TabIndex = 2;
-            this.btnAdicionar.Text = "Adicionar";
-            this.btnAdicionar.UseVisualStyleBackColor = true;
-            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            this.button2.Location = new System.Drawing.Point(3, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // lblFruta
+            // button3
             // 
-            this.lblFruta.AutoSize = true;
-            this.lblFruta.Location = new System.Drawing.Point(12, 73);
-            this.lblFruta.Name = "lblFruta";
-            this.lblFruta.Size = new System.Drawing.Size(35, 13);
-            this.lblFruta.TabIndex = 3;
-            this.lblFruta.Text = "label1";
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(164, 52);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
-            this.btnEliminar.TabIndex = 4;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            this.tableLayoutPanel1.SetColumnSpan(this.button3, 2);
+            this.button3.Location = new System.Drawing.Point(3, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(316, 306);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.lblFruta);
-            this.Controls.Add(this.btnAdicionar);
-            this.Controls.Add(this.txtFruta);
-            this.Controls.Add(this.lstFrutas);
+            this.ClientSize = new System.Drawing.Size(345, 319);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox lstFrutas;
-        private System.Windows.Forms.TextBox txtFruta;
-        private System.Windows.Forms.Button btnAdicionar;
-        private System.Windows.Forms.Label lblFruta;
-        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- C#/Form1.cs
+++ C#/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Programa_2._9
+namespace Programa_3._9
 {
     public partial class Form1 : Form
     {
@@ -19,31 +19,7 @@ namespace Programa_2._9
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            lstFrutas.Items.Add("Uva");
-            lstFrutas.Items.Add("Banana");
-        }
 
-        private void btnAdicionar_Click(object sender, EventArgs e)
-        {
-            lstFrutas.Items.Add(txtFruta.Text);
-
-            txtFruta.Text = string.Empty;
-
-        }
-
-        private void lstFrutas_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            if(lstFrutas.SelectedIndex!=-1)
-            lblFruta.Text = (string)lstFrutas.Items[lstFrutas.SelectedIndex];
-        }
-
-        private void btnEliminar_Click(object sender, EventArgs e)
-        {
-            int indice = lstFrutas.SelectedIndex;
-            if (indice!= -1)
-            {
-                lstFrutas.Items.RemoveAt(indice);
-            }
         }
     }
 }
